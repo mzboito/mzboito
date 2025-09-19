3475383b8ca8c8e7f644779abead8d38efe42cf3
mzboito <mzboito@gmail.com>
2025-09-19 14:00:19 +0000

    Deploying to gh-pages from @ mzboito/mzboito.github.io@8a678914db10281f5355982aa820ea4c98afcdfb 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mzboito/mzboito",
+    "data-repo": "mzboito/mzboito.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
